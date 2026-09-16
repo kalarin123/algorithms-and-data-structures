ff7e8efdfbd8b86cddf7751caec0f7ecdb3c011e
kalarin123 <69470641+kalarin123@users.noreply.github.com>
2026-09-16 21:41:47 +0300

    Обновить решение задачи 3 на Python

--- lab1/lab1_3.py
+++ lab1/lab1_3.py
@@ -1,12 +1,21 @@
-n = int(input())
-A = list(map(int, input().split()))
+import sys
 
-swaps = 0
 
-for i in range(n - 1):
-    for j in range(n - i - 1):
-        if A[j] > A[j + 1]:
-            A[j], A[j + 1] = A[j + 1], A[j]
-            swaps += 1
+def BubbleSort(A):
+    n = len(A)
 
-print(swaps)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if A[j] < A[j + 1]:
+                A[j], A[j + 1] = A[j + 1], A[j]
+
+    return A
+
+
+def main():
+    numbers = list(map(int, sys.stdin.buffer.read().split()))
+    print(*BubbleSort(numbers))
+
+
+if __name__ == "__main__":
+    main()
