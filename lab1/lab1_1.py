f05225c1bda913a43ab9f6e5b5bd31fd728690f6
kalarin123 <69470641+kalarin123@users.noreply.github.com>
2026-09-16 21:41:34 +0300

    Обновить решение задачи 1 на Python

--- lab1/lab1_1.py
+++ lab1/lab1_1.py
@@ -1,18 +1,23 @@
-def SelectionSort(A):
-    n = len(A)
+import sys
 
-    for i in range(n - 1):
+
+def SelectionSort(A):
+    for i in range(len(A) - 1):
         max_index = i
 
-        for j in range(i + 1, n):
+        for j in range(i + 1, len(A)):
             if A[j] > A[max_index]:
                 max_index = j
 
         A[i], A[max_index] = A[max_index], A[i]
 
+    return A
 
-A = list(map(int, input().split()))
 
-SelectionSort(A)
+def main():
+    numbers = list(map(int, sys.stdin.buffer.read().split()))
+    print(*SelectionSort(numbers))
 
-print(*A)
+
+if __name__ == "__main__":
+    main()
