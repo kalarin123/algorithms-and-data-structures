43f5fa184dbb1b1aa49d83c8c0684e6d32dd03ba
kalarin123 <69470641+kalarin123@users.noreply.github.com>
2026-09-16 21:21:41 +0300

    Добавить ЛР1 задача 4 на Python

---
+++ lab1/lab1_4.py
@@ -0,0 +1,46 @@
+import sys
+
+
+def Merge(A, left, mid, right):
+    temp = []
+    i = left
+    j = mid + 1
+
+    while i <= mid and j <= right:
+        if A[i] <= A[j]:
+            temp.append(A[i])
+            i += 1
+        else:
+            temp.append(A[j])
+            j += 1
+
+    while i <= mid:
+        temp.append(A[i])
+        i += 1
+
+    while j <= right:
+        temp.append(A[j])
+        j += 1
+
+    for k in range(len(temp)):
+        A[left + k] = temp[k]
+
+
+def MergeSort(A, left, right):
+    if left >= right:
+        return
+
+    mid = (left + right) // 2
+
+    MergeSort(A, left, mid)
+    MergeSort(A, mid + 1, right)
+    Merge(A, left, mid, right)
+
+
+data = list(map(int, sys.stdin.read().split()))
+n = data[0]
+A = data[1:1 + n]
+
+MergeSort(A, 0, n - 1)
+
+print(*A)
