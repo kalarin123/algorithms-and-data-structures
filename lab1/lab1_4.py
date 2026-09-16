edda76b478a0b2e754961899d6f32b5217ce107f
kalarin123 <69470641+kalarin123@users.noreply.github.com>
2026-09-16 21:41:53 +0300

    Обновить решение задачи 4 на Python

--- lab1/lab1_4.py
+++ lab1/lab1_4.py
@@ -1,46 +1,26 @@
 import sys
 
 
-def Merge(A, left, mid, right):
-    temp = []
-    i = left
-    j = mid + 1
+def BubbleSwapCount(A):
+    swaps = 0
+    n = len(A)
 
-    while i <= mid and j <= right:
-        if A[i] <= A[j]:
-            temp.append(A[i])
-            i += 1
-        else:
-            temp.append(A[j])
-            j += 1
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if A[j] > A[j + 1]:
+                A[j], A[j + 1] = A[j + 1], A[j]
+                swaps += 1
 
-    while i <= mid:
-        temp.append(A[i])
-        i += 1
-
-    while j <= right:
-        temp.append(A[j])
-        j += 1
-
-    for k in range(len(temp)):
-        A[left + k] = temp[k]
+    return swaps
 
 
-def MergeSort(A, left, right):
-    if left >= right:
-        return
+def main():
+    data = list(map(int, sys.stdin.buffer.read().split()))
+    n = data[0]
+    A = data[1:1 + n]
 
-    mid = (left + right) // 2
-
-    MergeSort(A, left, mid)
-    MergeSort(A, mid + 1, right)
-    Merge(A, left, mid, right)
+    print(BubbleSwapCount(A))
 
 
-data = list(map(int, sys.stdin.read().split()))
-n = data[0]
-A = data[1:1 + n]
-
-MergeSort(A, 0, n - 1)
-
-print(*A)
+if __name__ == "__main__":
+    main()
